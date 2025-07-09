93338cce0d4c196985ffca7f673b2d2645121774
Sumit kumar <sumitksr6223@gmail.com>
2025-07-09 22:22:32 +0530

    Update editPost.js

--- frontend/src/pages/editPost.js
+++ frontend/src/pages/editPost.js
@@ -43,7 +43,7 @@ export default function EditPost() {
 
   function handleDelete(id) {
     if (window.confirm('Are you sure you want to delete this post?')) {
-      fetch(`${BACKEND_URL}/api/v1/upload/posts/${id}`, {
+      fetch(`https://blogapp-yakt.onrender.com/api/v1/upload/posts/delete/${id}`, {
         method: 'DELETE',
         headers: {
           'Authorization': `Bearer ${localStorage.getItem('token')}`,
