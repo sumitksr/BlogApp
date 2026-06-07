5a31c3a608ce44fd6bfe738055abd69955767ee2
Sumit kumar <sumitksr4156@gmail.com>
2026-06-07 14:35:29 +0530

    change model

--- backend/controllers/chatbotController.js
+++ backend/controllers/chatbotController.js
@@ -12,7 +12,7 @@ exports.chatWithAI = async (req, res) => {
 
   try {
     const response = await axios.post(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         contents: [
           {
